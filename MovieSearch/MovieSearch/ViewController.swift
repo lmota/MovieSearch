3f3e72860f1480dd1e76b018420802e9a47252ff
Lopa Dharamshi <lopa.d.dharamshi@jpmchase.com>
2019-04-20 05:39:26 -0400

    First commit for project on Movie search

---
+++ MovieSearch/MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Dharamshi, Lopa D on 4/20/19.
+//  Copyright © 2019 Dharamshi Lopa D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
